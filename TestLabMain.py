33328174bda6d8d98599d6b36a08a568e824dec1
luten145 <61258117+luten145@users.noreply.github.com>
2023-05-09 16:24:33 +0900

    update2

--- TestLabMain.py
+++ TestLabMain.py
@@ -1,147 +1,91 @@
-#pip install requests hgtk
-import Util
-import requests, hgtk, random
 import re
 import json
-from konlpy.tag import Kkma
-import DicApi as dicapi
-import Util as util
 import os
-
 from tkinter import filedialog
 from tkinter import messagebox
 
-
-TAG = "TestLabMain"
-
-
-#시간
-#장소
-#이름
-
-place_score=[0 for i in range(100)]
-currentLine = 0
-
-def main():
-    # txt 파일을 json파일로 읽기
-    list_file = []                                          #파일 목록 담을 리스트 생성
-    files = filedialog.askopenfilenames(initialdir="/", \
+# txt 파일을 json파일로 읽기
+list_file = []      #파일 목록 담을 리스트 생성
+files = filedialog.askopenfilenames(initialdir="/", \
                                     title = "파일을 선택 해 주세요", \
                                     filetypes = (("*.txt","*txt"),("*.xls","*xls"),("*.csv","*csv")))
-    #files 변수에 선택 파일 경로 넣기
+#files 변수에 선택 파일 경로 넣기
+if files == '':
+    messagebox.showwarning("경고", "파일을 추가 하세요")    #파일 선택 안했을 때 메세지 출력
+print(files)    #files 리스트 값 출력
+with open(files[0], 'r', encoding='utf-8') as f:
+    data3 = json.load(f)
+# json 파일 내에 text 키의 값들만 읽어서 하나의 리스트로 만들기
+text_list= data3["task_result"]["text"]
+combined_text = "".join(text_list)
+text = [combined_text]
 
-    if files == '':
-        messagebox.showwarning("경고", "파일을 추가 하세요")    #파일 선택 안했을 때 메세지 출력
+text_list=str(text).split('\\n')
+text_str=str(text).replace("\\n"," ")
 
-    print(files)    #files 리스트 값 출력
+rows = 100
+cols = 3
+scoreTable = [[0 for j in range(cols)] for i in range(rows)]
 
-    with open(files[0], 'r', encoding='utf-8') as f:
-        data3 = json.load(f)
+print(text_list)
+print()
+print()
 
-    # json 파일 내에 text 키의 값들만 읽어서 하나의 리스트로 만들기
-    text_list= data3["task_result"]["text"]
-    combined_text = "".join(text_list)
-    text = [combined_text]
+placeKey = ["서울","부산","대구","인천","광주","대전","울산","경기","강원","충청","전라","경상","제주","세종","홀","웨딩","층"]
+place_list=[]
 
-    print(text)
-    analyze(str(text))
+nameKey = ['남','녀',"아들","딸",'의']
+name_list=[]
 
-    return 0
+for i in range(len(text_list)):
+    for j in placeKey:
+        if text_list[i].find(j) != -1:
+            place_list.append(text_list[i])
+            break
 
+    for j in nameKey:
+        if text_list[i].find(j) != -1:
+            name_list.append(text_list[i+1])
+            break
 
+#초기화
+year,month,day,week,hour,minute=0,0,0,0,0,0
 
+# 년도 추출
+year_str = re.search(r'\d+년', text_str)[0]
+year = int(re.search(r'\d+', year_str)[0])
 
-def analyze(text):
-    line_list = count_lines(text) # 라인 리스트
-    currentLine = line_list # 현재 라인을 전역변수에 저장
-    Np = 0
+# 월 추출
+month_str = re.search(r'\d+월', text_str)[0]
+month = int(re.search(r'\d+', month_str)[0])
 
-    for i in line_list: # 라인을 i에 넣습니다.
-        data = i
+# 일 추출
+day_str = re.search(r'\d+일', text_str)[0]
+day = int(re.search(r'\d+', day_str)[0])
 
-        spp = split(data) # 각 라인의 명사 리스트를 얻습니다. (Use KonlPy)
-        for j in spp: # 명사를 리스트를 반복합니다.
-            dd = dicapi.search(j) # 명사 검색시작
+# 요일 추출
+week = re.search(r'\d+년 \d+월 \d+일 (일|월|화|수|목|금|토)요일 (?:오후|오전|낮)?\s?(\d{1,2})\s?[시:]\s?(\d{0,2})\s?(?:분)?', text_str)
 
-            if(dd != -1):
-                Util.Log(TAG,"-------------------")
-                Util.Log(TAG,"검색한 단어 : "+dd[0])
-                Util.Log(TAG,"단어의 뜻 : "+dd[1])
-                Util.Log(TAG,"-------------------")
-                place_score[Np] += scoring(dd)
-
-
-            else:
-                Util.Log(TAG,"사전에서 단어를 찾을 수 없습니다!")
-        Np += 1
-
-    for i in range(Np):
-        print("LineCount : ",i,place_score[i]," || ",get_line(text,i))
-
-    max_place_score = 0
-    for i in range(Np):
-        if max_place_score < place_score[i]:
-            max_place_score = place_score[i]
-            index = i
-
-
-    print("장소 | 최고점 : %d" %max_place_score,end = '')
-    print(" | index : %d" %index,end = '')
-    print(" | 데이터 : ", get_line(text,index))
-
-
-global placeKey
-placeKey = ['건물','공간','위치','장소','홀','예식장']
-#지명
-
-# 한 줄에 대한 점수를 채점합니다.
-def scoring(data):
-    place = 0
-    # 장소 판별
-
-    for i in placeKey:
-        if data[0].find(i) != -1:
-            place +=1
-
-        if data[1].find(i) != -1:
-            place +=1
-
-    return place
-
-def count_lines(text):
-    return text.split('\\n')
-
-def get_line(text,line_number):
-    line_content = text.split("\\n")
-    return line_content[line_number]
-
-def split(data):
-    kkma = Kkma()
-    return kkma.nouns(data)
+# 시간 추출
+time_matches = re.findall(r'(?:오후|오전|낮)?\s?(\d{1,2})\s?[시:]\s?(\d{0,2})\s?(?:분)?', text_str)[0]
+hour = int(time_matches[0])
+if '오후' in text_list:
+    hour += 12
+minute = int(time_matches[1]) if time_matches[1] else 0
 
 
 
+print(place_list)
+print(name_list)
+print("----------")
+print("년:", year, type(year))
+print("월:", month, type(month))
+print("일:", day, type(day))
+print("요일:", week, type(week))
+print(hour,":",minute)
 
-
-# 1.줄 분리
-# 2.줄 평가
-# 3.순위 쿼리
-# 4.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-main()
-
-exit()
+"""
+정규표현 방식을 배열화 시켜서
+place_key,name_key랑 유사하게 동작하여
+날짜에 대한 배열 추출
+"""
