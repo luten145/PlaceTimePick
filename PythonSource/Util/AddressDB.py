03fe07a19a2785601605e4b8d03bae875b118031
luten145 <61258117+luten145@users.noreply.github.com>
2023-05-21 00:21:45 +0900

    AddressDB Update TODO List

--- PythonSource/Util/AddressDB.py
+++ PythonSource/Util/AddressDB.py
@@ -15,6 +15,7 @@ class AddressDB:
         self.f.close()
         pass
 
+
     def getAdressList(self,Find=0, AddressNum=1, AddressGet=1):
         l = self.SearchAddress(self.AddressList,Find,AddressNum,AddressGet)
         return l
@@ -22,12 +23,16 @@ class AddressDB:
 
     def SearchAddress(self,AddressList, Find=0, AddressNum=1, AddressGet=1):
         Address=[[]for i in range(2)]
+        if(Find == '전북'):
+            Find = '전라북도'
         if Find != 0:
             if AddressGet == FIND_CITY:
                 for i in AddressList:
                     if Find in i[AddressNum]:
                         Address[0].append(i[AddressGet][0:2]); Address[1].append(i[AddressGet])
                 Address[0]=list(set(Address[0])); Address[1]=list(set(Address[1]))
+                Address[0].append('전북')
+                Address[0].append('전남')
                 return Address
             
             elif AddressGet == FIND_DISTRICT or AddressGet == FIND_TOWN:
@@ -91,6 +96,8 @@ class AddressDB:
                 for i in AddressList:
                     Address[0].append(i[AddressGet][0:2]); Address[1].append(i[AddressGet])
                 Address[0]=list(set(Address[0])); Address[1]=list(set(Address[1]))
+                Address[0].append('전북')
+                Address[0].append('전남')
                 return Address
             
             elif AddressGet == FIND_DISTRICT or AddressGet == FIND_TOWN:
@@ -147,17 +154,19 @@ class AddressDB:
 
 
 
-    # TODO 1 : 핵심 단어만 리스트화하는 함수 만들기
+    # ODO 1 : 핵심 단어만 리스트화하는 함수 만들기 OK
     #  EX)서울 특별시 -> 서울 ,강원도 -> 강원
     #  방식은 서울 특별시와 같은 리스트를 넣으면 변환된 리스트가 나오게 하는 함수를 새로 만들거나
     #  기존 함수에 플래그 인수 추가 중 선택
     #  SearchAdress(AddressList,"경상북도",0)  # 출력 : 서울 특별시 , 강원도, 제주특별자치도
     #  SearchAdress(AddressList,"경상북도",1)  # 출력 : 서울,강원,대구
 
-    # TODO 2 : 한글자인거는 뒤까지 툴력 하도록  변경 ex) 중구 -> 중 (X)  , 중구 -> 중구
-    # TODO 3 : 시 군구 에서 '성남시 수정', '성남시 분당' 같은 경우 성남, 분당 , 성남, 수정으로 나누어서 리스트 만들기
-    # TODO 4 : 도로명의 경우 뒷 숫자 빼고 리스트 말들고 숫자는 따로 리스트 얻는함수 만들기
-    # TODO 5 : 서울입력 -> 서울특별시 출력되는 함수 만들기
+    # ODO 2 : 한글자인거는 뒤까지 툴력 하도록  변경 ex) 중구 -> 중 (X)  , 중구 -> 중구
+    # ODO 3 : 시 군구 에서 '성남시 수정', '성남시 분당' 같은 경우 성남, 분당 , 성남, 수정으로 나누어서 리스트 만들기
+    # ODO 4 : 도로명의 경우 뒷 숫자 빼고 리스트 말들고 숫자는 따로 리스트 얻는함수 만들기
+    # ODO 5 : 서울입력 -> 서울특별시 출력되는 함수 만들기
+    # TODO 5 : 전북 전남 경북 경남 안됨 => 전북,전남 입력시 전라북도,전라남도 나와야 하고 CITY 출력 시 전라북도 => 전라,전북 모두 나오도록 하기
+    # TODO 6 : 지번정보 검색 불가 => 지번도 사용 가능하도록 하기 + 지번과 도로명주소는 동시에 검색되면 안됨 인수로 이를 구분하도록 하기
 
 
 
