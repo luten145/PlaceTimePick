4ab206344f3f755acfab46867e987d7bb7c0f5fc
luten145 <61258117+luten145@users.noreply.github.com>
2023-06-12 16:29:16 +0900

    [StringUtil] Add method
    extractWords : Returns a list of words containing the keyword.
    extractNumber : Returns a list of numbers in a string.
    getTextList : Returns data in Json format in the form of a text list.

--- PythonSource/Util/StringUtil.py
+++ PythonSource/Util/StringUtil.py
@@ -1,18 +1,22 @@
+import re
 from difflib import SequenceMatcher
 
-def similarity(a, b):
+
+def getSimilarity(a, b):
     return SequenceMatcher(None, a, b).ratio()
+
 
 def countPattern(string, pattern):
     if string == '' or pattern == '':
         return 0
     count = string.count(pattern)
-
     return count
+
 
 def split_string(text):
     words = text.split()
     return words
+
 
 def generateCombinations(text):
     words = text.split()
@@ -24,6 +28,7 @@ def generateCombinations(text):
                 result.append(' '.join(combination))
     return result
 
+
 def removeSubstring(text, substring):
     words = text.split()
     result = []
@@ -32,5 +37,23 @@ def removeSubstring(text, substring):
             result.append(word)
     return ' '.join(result)
 
+
 def containsSubstring(text, substring):
     return substring in text
+
+
+# Find words in a line of text that contain the keyword word
+def extractWords(text, keyword):  # text : TextLine , keyword : word to find
+    result = []
+    for word in text.split():
+        if keyword in word:
+            result.append(word)
+    return result
+
+
+def extractNumbers(text):
+    return re.findall(r'\d+(?:-\d+)?', text)
+
+
+def getTextList(raw):  # Json text to textlist
+    return str(["".join(raw["task_result"]["text"])]).split('\\n')
