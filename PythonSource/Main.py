b0fb4b3d074e330a5a189cb84068513c7000dc84
luten145 <61258117+luten145@users.noreply.github.com>
2023-06-12 16:21:53 +0900

    [Main] Code optimization

--- PythonSource/Main.py
+++ PythonSource/Main.py
@@ -1,31 +1,33 @@
-import PythonSource.Engine.EngineManager as engineManager
-import PythonSource.UI.UIMain as ui
+from PythonSource.UI import UIMain
 from PythonSource.UI.UIListener import FrameworkListener
-import PythonSource.Util.JsonUtil as jsonUtil
+from PythonSource.Util import JsonUtil
+from PythonSource.Engine import EngineManager
 
 TAG = "TestLabMain"
 
-jsonManager = jsonUtil.JsonManager()
+jsonManager = JsonUtil.JsonManager()
 
 
 class UIListener(FrameworkListener):
     def onTkinterEvent(self, text) -> bool:
         mainEngine.tkinterHandler(text)
         return False
+
     def onJsonOpenEvent(self) -> bool:
         data = jsonManager.showFileManager()
         mainEngine.jsonHandler(data)
         return False
 
-uiManager = ui.UIManager(UIListener())
-mainEngine = engineManager.MainEngine(uiManager.getDataEventListener())
+
+uiManager = UIMain.UIManager(UIListener())
+mainEngine = EngineManager.MainEngine(uiManager.getDataEventListener())
+
 
 def main():
     index = 5
     listener = uiManager.getDataEventListener()
-    listener.onSetDataEvent(index, "This is UI Event Index " + str(index)) # UI 이벤트 테스트 코드
+    listener.onSetDataEvent(index, "This is UI Event Index " + str(index))  # UI 이벤트 테스트 코드
     uiManager.root.mainloop()
-    pass
 
-    print("**")
+
 main()
